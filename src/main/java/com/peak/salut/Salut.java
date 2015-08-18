b53ddfbc7a4267bc352ac3fee45ecdced2c4df06
Matt Allen <matt.allen@3sidedcube.com>
2015-08-18 09:59:11 +0100

    NPE fix

--- src/main/java/com/peak/salut/Salut.java
+++ src/main/java/com/peak/salut/Salut.java
@@ -599,7 +599,7 @@ public class Salut implements WifiP2pManager.ConnectionInfoListener
 			cleanUpDeviceConnection(false);
 			disconnectFromDevice();
 		}
-		else
+		else if (registeredHost.serviceAddress != null)
 		{
 			startRegistrationForClient(new InetSocketAddress(registeredHost.serviceAddress, SALUT_SERVER_PORT));
 		}
